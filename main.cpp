8b054ee21d697f5e0e7cfdd39062dfe9703189f8
NasirCrossGriffin <43095962+NasirCrossGriffin@users.noreply.github.com>
2024-03-19 13:25:42 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <list>
+#include <iostream>
+#include <limits.h>
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+#include "server.h"
+#include <fstream>
+#include <thread>
+#include <string>
+#include "client.cpp"
+using namespace std;
+
+	int main(int argc, char* argv[])
+	{
+		/*Gather the list and sort it
+		Written by Nasir Cross-Griffin*/
+
+		list<Client*> clients;
+
+		int i = 0;
+		while (i < 10)
+		{
+			clients.push_back(new Client("Matthew", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			clients.push_back(new Client("Sarah", 1000 + (rand() % 8999), 20 + (rand() % 40), "FEMALE"));
+			clients.push_back(new Client("Jason", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			clients.push_back(new Client("Seth", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			clients.push_back(new Client("Lisa", 1000 + (rand() % 8999), 20 + (rand() % 40), "FEMALE"));
+			clients.push_back(new Client("Trina", 1000 + (rand() % 8999), 20 + (rand() % 40), "FEMALE"));
+			clients.push_back(new Client("Maxwell", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			clients.push_back(new Client("Roxanne", 1000 + (rand() % 8999), 20 + (rand() % 40), "FEMALE"));
+			clients.push_back(new Client("Timothy", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			clients.push_back(new Client("Reece", 1000 + (rand() % 8999), 20 + (rand() % 40), "MALE"));
+			
+			i++;
+		}
+
+
+
+		//sort(clients.begin(), clients.end(), Client::compareClients);
+
+		//Create a new file
+		try {
+			ofstream NewFile("C:\\Users\\nasir\\Documents\\ListOfClients.txt");
+			std::cout << "File created\n";
+			NewFile.close();
+		}
+		catch (exception& e) {
+			std::cout << "Cannot create file";
+		}
+
+		
+
+		ofstream MyFile("C:\\Users\\nasir\\Documents\\ListOfClients.txt");
+
+		//Print out all members of the list (Will be changed to append to file)
+		list<Client*>::iterator it;
+
+		for (it = clients.begin(); it != clients.end(); it++)
+			MyFile << (**it).toString() << "\n";
+
+		MyFile.close();
+		/*for (int i; i < NewList.size(); i++)
+		{
+			cout << it.next();
+		} */
+
+		Server();
+
+		return 0;
+
+
+	}
---
+++ server.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include "server.h"
+#include <iostream>
+#include <fstream>
+
+using boost::asio::ip::tcp;
+
+
+	void Server()
+	{
+		try {
+			//Opens an IO context that boost uses to keep input and output on the system
+			boost::asio::io_context io_context;
+
+			//This acceptor accepts connections on port 1337
+			tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 1337));
+
+			while (true) {
+				//Waits for a connections and stores that connection in the socket
+				std::cout << "Accepting connections on port 1337!\n";
+				tcp::socket socket(io_context);
+				acceptor.accept(socket);
+
+				std::cout << "Client connected! Sending data!\n";
+				std::string hello_message = "Welcome to testing enviroment server!\n";
+				std::ifstream MyReadFile("C:\\Users\\nasir\\Documents\\ListOfClients.txt");
+				// Use a while loop together with the getline() function to read the file line by line
+				//while (getline(MyReadFile, myText)) {
+					// Output the text from the file
+					//std::cout << myText;
+				//}
+
+				std::stringstream buffer;
+				buffer << MyReadFile.rdbuf();
+
+				MyReadFile.close();
+				boost::system::error_code error;
+
+				//boost::asio::write(socket, boost::asio::buffer(hello_message), error);
+				boost::asio::write(socket, boost::asio::buffer(buffer.str()), error);
+			}
+		}
+		catch (std::exception e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
