867967526dccf74bd9da3dd39ccfd5f239614874
NasirCrossGriffin <43095962+NasirCrossGriffin@users.noreply.github.com>
2024-04-04 12:51:06 -0400

    Add files via upload

---
+++ parallel_id.h
@@ -0,0 +1,16 @@
+#ifndef EMPLOYEE_GENERATOR_H
+#define EMPLOYEE_GENERATOR_H
+
+#include <pthread.h>
+
+// Number of threads to generate employee IDs
+const int NUM_THREADS = 500;
+
+// Function executed by each thread to generate a random 9-digit employee ID
+void *generate_employee_id(void *arg);
+
+// Function to generate employee IDs using multiple threads
+int generate_employee_ids_multithreaded();
+
+#endif // EMPLOYEE_GENERATOR_H
+
